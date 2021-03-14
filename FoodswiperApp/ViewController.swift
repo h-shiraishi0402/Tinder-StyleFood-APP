8e8fb749e0775e8c2429fa0d4be990b88054fda1
HiroyukiShiraishi <hiro19900402@gmail.com>
2021-03-14 21:21:17 +0900

    Initial Commit

---
+++ FoodswiperApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodswiperApp
+//
+//  Created by 白石裕幸 on 2021/03/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
